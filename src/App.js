0f3cb46e0b4fd9efa44f87dcd9e46d24b444acf9
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-06-25 20:23:31 +0300

    latest updates for the starter files + adding forms

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,16 +22,29 @@ const initialFriends = [
   },
 ];
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friendsList, setFriendsList] = useState(initialFriends);
+  function handleShowAddFriendForm() {
+    setShowAddFriend((show) => !show);
+  }
+  function handleAddingNewFriend(friend) {
+    setFriendsList((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
+        <FriendsList friends={friendsList} />
+        {showAddFriend && <AddFriendForm addFriend={handleAddingNewFriend} />}
+        <ReusableButton clickFunc={handleShowAddFriendForm}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </ReusableButton>
       </div>
+      <SplitBillForm />
     </div>
   );
 }
-function FriendsList() {
-  const friends = initialFriends;
+function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map((singleFriend) => {
@@ -55,9 +71,74 @@ function SingleFriend({ friendData }) {
       ) : (
         <div></div>
       )}
-      <button className="button">Select</button>
+      <ReusableButton>Select</ReusableButton>
     </li>
   );
 }
+function AddFriendForm({ addFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+  function handleSubmitAddForm(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const GenerativeId = uuidv4();
+    const newFriend = {
+      id: GenerativeId,
+      name,
+      balance: 0,
+      image: `${image}?${GenerativeId}`,
+    };
+    addFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+  return (
+    <form onSubmit={(e) => handleSubmitAddForm(e)} className="form-add-friend">
+      <label>👬 Friend name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
 
+      <label>🌄 Image Url</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(event) => setImage(event.target.value)}
+      />
+
+      <ReusableButton>Add</ReusableButton>
+    </form>
+  );
+}
+function SplitBillForm() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split Bill With XX</h2>
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>👨🏻‍💻 Your Expense</label>
+      <input type="text" />
+
+      <label>👬 X's Expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value={"user"}>You</option>
+        <option value={"friend"}>X</option>
+      </select>
+      <ReusableButton>Split bill</ReusableButton>
+    </form>
+  );
+}
+function ReusableButton({ children, clickFunc }) {
+  return (
+    <button onClick={clickFunc} className="button">
+      {children}
+    </button>
+  );
+}
 export default App;
